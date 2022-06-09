83ad99d7bf2ae0e3bf2fe2510d578a2af8d9606d
Dmitry Yakovlev <Dimon1998daf@mail.ru>
2022-06-09 16:49:07 +0600

    Lesson2

---
+++ WebApplication3/WebApplication3/Controllers/ProductListController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductListController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            return View(ProductController._catalog);
+        }
+
+    }
+}
