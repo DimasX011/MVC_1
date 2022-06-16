3e1698222ba7a9d62cfd61058d717b6088056f5a
Dmitry Yakovlev <Dimon1998daf@mail.ru>
2022-06-16 16:23:28 +0600

    L5v1

---
+++ MVC_L5/WebApplication3/Models/ProductsCatalog.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+using WebApplication3;
+    
+namespace WebApplication3.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+        public FormUrlEncodedContent ToFormData()
+        {
+            return new FormUrlEncodedContent(new[]
+            {
+            new KeyValuePair<string, string>(nameof(Name), Name),
+            new KeyValuePair<string, string>(nameof(Id), Id.ToString()),
+            new KeyValuePair<string, string>(nameof(Description), Description),
+            new KeyValuePair<string, string>(nameof(Type), Type)
+        });
+        }
+    }
+
+}
