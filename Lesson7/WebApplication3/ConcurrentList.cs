2d9d8cbb6a1cbdfd7e19e1171209016709f37d75
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-06-25 12:22:45 +0600

    Lesson7

---
+++ Lesson7/WebApplication3/ConcurrentList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Concurrent;
+using WebApplication3.Models;
+
+namespace WebApplication3
+{
+    public class ConcurrentList<T> : List<T>
+    {
+        
+    }
+}
---
+++ Lesson7/WebApplication3/Services/IsWorkedBackgroundService.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace WebApplication3.Services;
+
+public class IsWorkedBackgroundService : BackgroundService
+{
+    private EmailService _emailService = new ();
+
+    public IsWorkedBackgroundService(EmailService emailService)
+    {
+        _emailService = emailService;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var timer = new PeriodicTimer(TimeSpan.FromMinutes(60));
+        Stopwatch sw = Stopwatch.StartNew();
+        while (await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            _emailService.SendEmail("Dimon1998daf@mail.ru", "Проверка работоспособности сервиса", "Сервис работает" + DateTime.Now,"Службы ", "Контроля");
+        }
+    }
+
+}
